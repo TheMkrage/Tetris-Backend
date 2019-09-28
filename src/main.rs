e2f970de47dee9919a6ee43ea2bb3da7f9523c54
Matthew Krager <matthewkrager@gmail.com>
2019-09-28 14:37:01 -0700

    websockets

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 extern crate rocket_contrib;
+extern crate ws;
+
+use std::cell::Cell;
+use std::rc::Rc;
 
 use rocket_contrib::serve::StaticFiles;
 use std::io;
+use ws::{listen, CloseCode, Handler, Handshake, Message, Result, Sender};
+
+struct Server {
+    out: Sender,
+    count: Rc<Cell<u32>>,
+}
+
+impl Handler for Server {
+    fn on_open(&mut self, shake: Handshake) -> Result<()> {
+        println!("open");
+        return Ok(self.count.set(self.count.get() + 1));
+    }
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        println!("message");
+        //print!("{}", msg.into_text().unwrap());
+        // Echo message back
+        return self.out.broadcast(msg);
+    }
+
+    fn on_close(&mut self, code: CloseCode, reason: &str) {
+        match code {
+            CloseCode::Normal => println!("The client is done with the connection."),
+            CloseCode::Away => println!("The client is leaving the site."),
+            _ => println!("The client encountered an error: {}", reason),
+        }
+    }
+}
 
 fn rocket() -> rocket::Rocket {
     // Mount "/" route to serve contents of /static
@@ -10,5 +41,11 @@ fn rocket() -> rocket::Rocket {
 }
 
 fn main() {
-    rocket().launch();
+    let count = Rc::new(Cell::new(0));
+    listen("127.0.0.1:3012", |out| Server {
+        out: out,
+        count: count.clone(),
+    })
+    .unwrap();
+    //rocket().launch();
 }
